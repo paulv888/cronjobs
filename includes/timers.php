0c4ab5d221625c88b96a10735f6432cfe80f7922
paulv888 <paulv888@inbox.com>
2015-05-27 00:03:30 -0500

    fix debug_timers

--- includes/timers.php
+++ includes/timers.php
@@ -65,9 +65,7 @@ function RunTimers(){
 						$message = executeCommand(MY_DEVICE_ID, MESS_TYPE_SCHEME, array( 'schemeID' => $timer['schemeID'], 'loglevel' => LOGLEVEL_NONE));
 					}
 					if (!DEBUG_TIMERS) $result = ob_get_clean();
-					if ($timer['priorityID'] != '99') logEvent($log = array('inout' => COMMAND_IO_BOTH, 'callerID' => MY_DEVICE_ID, 'deviceID' => MY_DEVICE_ID, 'commandID' => COMMAND_RUN_SCHEME, 
-								 'data' => GetSchemaName($timer['schemeID']), 'message' => $message ));
-					
+					if ($timer['priorityID'] != '99') logEvent($log = array('inout' => COMMAND_IO_BOTH, 'callerID' => MY_DEVICE_ID, 'deviceID' => MY_DEVICE_ID, 'commandID' => COMMAND_RUN_SCHEME, 'data' => GetSchemaName($timer['schemeID']), 'message' => $message ));
 					$mysql="UPDATE `ha_timers` ".
 						" SET last_run_date = '". date("Y-m-d H:i:s")."' WHERE `ha_timers`.`id` = ".$timer['id'] ;
 					if (DEBUG_TIMERS) echo date("Y-m-d H:i:s").": ".$mysql.CRLF;
@@ -119,11 +117,11 @@ function GetSchemaName($schemaID) {
 function UpdateTimers($dummy) {
 // PHP Command Dummy parm
 	$devstatusrows = FetchRows("SELECT deviceID, timerMinute, timerDate, timerRemaining FROM ha_mf_monitor_status  WHERE timerMinute > 0");
-	if (DEBUG_HA) print_r($devstatusrow);
+	//if (DEBUG_TIMERS) print_r($devstatusrows);
 	$feedback = "";
 	if ($devstatusrows) {
 		foreach ($devstatusrows as $devstatusrow) {
-			if (DEBUG_HA) print_r($devstatusrow);
+			if (DEBUG_TIMERS) print_r($devstatusrow);
 			if ($testvalue[] = $devstatusrow['timerMinute'] > 0 && timeExpired($devstatusrow['timerDate'], $devstatusrow['timerMinute'])) {
 				$feedback['SendCommand:'.$devstatusrow['deviceID']]=SendCommand(MY_DEVICE_ID, array( 'deviceID' => $devstatusrow['deviceID'], 'commandID' => COMMAND_OFF));
 			} else {
