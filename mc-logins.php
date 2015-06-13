f8391c79f46395d88aab8432e045e94b94b0541e
paulv888 <paulv888@inbox.com>
2015-06-13 16:15:27 -0500

    update

--- mc-logins.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("GMAIL_USER", "myvlohome@gmail.com");
-define("GMAIL_PASSWORD", "Mypassword01");
-define("CLARIFY_USER","commandalkon\pvloon");
-define("CLARIFY_PASSWORD","Vanille03");
-?>
---
+++ mc-updatevalues.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?php
+require_once 'mc-includes.php';
+
+define("MY_DEVICE_ID", 219);
+
+echo(updateValuesNew());
+?>
