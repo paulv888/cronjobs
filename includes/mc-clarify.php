2045a25aab42ef82d2ea698ba3fb901e3ef617aa
paulv888 <paulv888@inbox.com>
2015-06-21 11:13:23 -0500

    Read last 35 days mods

--- includes/mc-clarify.php
+++ includes/mc-clarify.php
@@ -29,16 +29,11 @@ function clarify($init = "CHANGES") {
 			$text.= date("Y-m-d H:i:s").": ".getCases("CaseToCR")." Cases/CR's Read".CRLF; 		// Initialize with all Cases
 			$text.= date("Y-m-d H:i:s").": ".storeCR(formatData())."  CRs Upserted.".CRLF;
 		}
+		$text.= lastXDays("Last35Days");
 		echo $text;
 		PDOupsert("mc_clarify_import", Array('description' => $init, 'result' => str_replace("</br>","",$text)), Array('description' => $init));
 	} else {
-		$result = getCases("Last8Days");							// No Distinct - Last 3 days
-		$text = date("Y-m-d H:i:s").": ".$result." Cases Read".CRLF;
-		if ($result != "ERROR:") {
-			$text = date("Y-m-d H:i:s").": ".$text." Cases/CR's Read".CRLF;			// No Distinct - Last 3 days
-			$text.= date("Y-m-d H:i:s").": ".storeCase(formatData())." Cases Upserted.".CRLF;
-			$text.= date("Y-m-d H:i:s").": ".storeCR(formatData())." CRs Upserted.".CRLF;
-		} 
+		$text = lastXDays("Last2Days");
 		echo $text;
 		PDOupsert("mc_clarify_import", Array('description' => $init, 'result' => str_replace("</br>","",$text)), Array('description' => $init));
 	}
@@ -46,6 +41,63 @@ function clarify($init = "CHANGES") {
 
 	if (DEBUG_CLARIFY) echo "</pre>";
 }
+
+function lastXDays($qname) {
+
+		$result = getCases($qname);							// No Distinct - Last 3 days
+		if ($result != "ERROR:") {
+			$text = date("Y-m-d H:i:s").": ".$result." Cases/CR's Read".CRLF;			// No Distinct - Last 3 days
+			$text.= date("Y-m-d H:i:s").": ".storeCase(formatData())." Cases Upserted.".CRLF;
+			$text.= date("Y-m-d H:i:s").": ".storeCR(formatData())." CRs Upserted.".CRLF;
+		} 
+		
+		$mysql = 'DELETE FROM `mc_cases_month` WHERE period = CAST( DATE_FORMAT( NOW( ) , "%Y-%m-01" ) AS DATE )  ';
+		if (!$result = mysql_query($mysql)){
+						mySqlError($mysql);
+						return false;
+		}
+		$mysql = 'INSERT INTO `mc_cases_month`(`period`, `in_service`, `opened`, `closed`) VALUES (CAST( DATE_FORMAT( NOW( ) , "%Y-%m-01" ) AS DATE )  ,0,0,0)';
+		if (!$result = mysql_query($mysql)){
+						mySqlError($mysql);
+						return false;
+		}
+		$mysql = 'INSERT INTO `mc_cases_month`(`period`, `in_service`, `opened`, `closed`) VALUES (CAST( DATE_FORMAT( NOW( ) , "%Y-%m-01" ) AS DATE )  ,1,0,0)';
+		if (!$result = mysql_query($mysql)){
+						mySqlError($mysql);
+						return false;
+		}
+		
+		$mysql = 'UPDATE `mc_cases_month` a JOIN (
+						SELECT count( * ) AS opened, CAST( DATE_FORMAT( NOW( ) , "%Y-%m-01" ) AS DATE ) AS period, in_service
+						FROM `mc_cases`
+						WHERE Create_Date >= CAST( DATE_FORMAT( NOW( ) , "%Y-%m-01" ) AS DATE )
+						GROUP BY in_service
+						)b ON a.period = b.period
+						AND a.in_service = b.in_service
+						SET a.opened = b.opened WHERE 1 ';
+		if (!$result = mysql_query($mysql)){
+						mySqlError($mysql);
+						return false;
+		}
+		$text.= date("Y-m-d H:i:s").": ".mysql_affected_rows()." Monthly count updated".CRLF;
+
+		$mysql = 'UPDATE `mc_cases_month` a JOIN (
+						SELECT count( * ) AS closed, CAST( DATE_FORMAT( NOW( ) , "%Y-%m-01" ) AS DATE ) AS period, in_service
+						FROM `mc_cases`
+						WHERE Close_Date >= CAST( DATE_FORMAT( NOW( ) , "%Y-%m-01" ) AS DATE )
+						GROUP BY in_service
+						)b ON a.period = b.period
+						AND a.in_service = b.in_service
+						SET a.closed = b.closed WHERE 1 ';
+		if (!$result = mysql_query($mysql)){
+						mySqlError($mysql);
+						return false;
+		}
+		$text.= date("Y-m-d H:i:s").": ".mysql_affected_rows()." Monthly count updated".CRLF;
+		
+	return $text;
+}
+
 
 function getCases($query) {
 
@@ -142,8 +194,11 @@ function getCases($query) {
 	case "OpenCases": 
 		$fields_string = "query=268440017&obj_type=case&parameters=Closed%2CMC%25&sqlStmt=SELECT%2520t0.id_number%2520%2522Case%2520ID%2522%252C%2520t4.login_name%2520%2522Owner%2522%252C%2520t0.title%2520%2522Case%2520Title%2522%252C%2520t1.title%2520%2522Condition%2522%252C%2520t5.title%2520%2522Status%2522%252C%2520t6.title%2520%2522Case%2520Type*%2522%252C%2520t7.site_id%2520%2522Site%2520ID%2522%252C%2520t7.name%2520%2522Site%2520Name%2522%252C%2520t8.title%2520%2522Priority*%2522%252C%2520t9.title%2520%2522Severity*%2522%252C%2520t10.first_name%2520%2522Contact%2520First%2520Name%2522%252C%2520t10.last_name%2520%2522Contact%2520Last%2520Name%2522%252C%2520CONVERT%2528CHAR%252C%2520t0.creation_time%2529%2520%2522CONPREFIX_Create%2520Date%2522%252C%2520t11.login_name%2520%2522Originator%2522%252C%2520t12.org_id%2520%2522Org%2520ID%2522%252C%2520t7.region%2520%2522Site%2520Region%2522%252C%2520CONVERT%2528CHAR%252C%2520t13.close_date%2529%2520%2522CONPREFIX_Close%2520Date%2522%252C%2520t0.x_customer_issue_no%2520%2522Cust%2520Issue%2522%252C%2520t3.part_number%2520%2522Part%2520Number%2522%252C%2520t0.x_module%2520%2522Module%2522%252C%2520CONVERT%2528CHAR%252C%2520t0.objid%2529%2520%2522objid%2522%2520FROM%2520table_case%2520t0%2520LEFT%2520OUTER%2520JOIN%2520table_mod_level%2520t2%2520ON%2520t0.case_prt2part_info%2520%253D%2520t2.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_part_num%2520t3%2520ON%2520t2.part_info2part_num%2520%253D%2520t3.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_close_case%2520t13%2520ON%2520t0.objid%2520%253D%2520t13.last_close2case%2520%252C%2520table_condition%2520t1%252C%2520table_user%2520t4%252C%2520table_gbst_elm%2520t5%252C%2520table_gbst_elm%2520t6%252C%2520table_site%2520t7%252C%2520table_gbst_elm%2520t8%252C%2520table_gbst_elm%2520t9%252C%2520table_contact%2520t10%252C%2520table_user%2520t11%252C%2520table_bus_org%2520t12%2520WHERE%2520%2528t0.case_state2condition%2520%253D%2520t1.objid%2529%2520AND%2520%2528t0.case_owner2user%2520%253D%2520t4.objid%2529%2520AND%2520%2528t0.casests2gbst_elm%2520%253D%2520t5.objid%2529%2520AND%2520%2528t0.calltype2gbst_elm%2520%253D%2520t6.objid%2529%2520AND%2520%2528t0.case_reporter2site%2520%253D%2520t7.objid%2529%2520AND%2520%2528t7.primary2bus_org%2520%253D%2520t12.objid%2529%2520AND%2520%2528t0.respprty2gbst_elm%2520%253D%2520t8.objid%2529%2520AND%2520%2528t0.respsvrty2gbst_elm%2520%253D%2520t9.objid%2529%2520AND%2520%2528t0.case_reporter2contact%2520%253D%2520t10.objid%2529%2520AND%2520%2528t0.case_originator2user%2520%253D%2520t11.objid%2529%2520AND%2520%2528t1.title%2520%2521%253D%2520%257B0%257D%2520AND%2520t3.part_number%2520like%2520%257B1%257D%2529&strClauses=%255B%2522update%257C268454224%257C268435483%257Cis%2520not%2520equal%2520to%257CClosed%257C0%257C0%257Ccase_state2condition%253Atitle%257C1%2522%252C%2522update%257C268454223%257C0%257Cjoins%2520groups%257CAND%257C0%257C0%257C%257C2%2522%252C%2522update%257C268454222%257C268435496%257Cstarts%2520with%257CMC%257C0%257C0%257C%2528LEFT%2520OUTER%2529%2520case_prt2part_info%253Apart_info2part_num%253Apart_number%257C3%2522%255D";
 		break;
-	case "Last8Days":
-		$fields_string = "query=268440603&obj_type=case&parameters=8%2CMC%25&sqlStmt=SELECT%2520t0.id_number%2520%2522Case%2520ID%2522%252C%2520t4.login_name%2520%2522Owner%2522%252C%2520t0.title%2520%2522Case%2520Title%2522%252C%2520t5.title%2520%2522Condition%2522%252C%2520t6.title%2520%2522Status%2522%252C%2520t7.title%2520%2522Case%2520Type*%2522%252C%2520t8.site_id%2520%2522Site%2520ID%2522%252C%2520t8.name%2520%2522Site%2520Name%2522%252C%2520t9.title%2520%2522Priority*%2522%252C%2520t10.title%2520%2522Severity*%2522%252C%2520t11.first_name%2520%2522Contact%2520First%2520Name%2522%252C%2520t11.last_name%2520%2522Contact%2520Last%2520Name%2522%252C%2520CONVERT%2528CHAR%252C%2520t0.creation_time%2529%2520%2522CONPREFIX_Create%2520Date%2522%252C%2520t12.login_name%2520%2522Originator%2522%252C%2520t13.org_id%2520%2522Org%2520ID%2522%252C%2520t8.region%2520%2522Site%2520Region%2522%252C%2520CONVERT%2528CHAR%252C%2520t14.close_date%2529%2520%2522CONPREFIX_Close%2520Date%2522%252C%2520t0.x_customer_issue_no%2520%2522Cust%2520Issue%2522%252C%2520t3.part_number%2520%2522Part%2520Number%2522%252C%2520t0.x_module%2520%2522Module%2522%252C%2520t17.id_number%2520%2522CR%2520ID%2522%252C%2520t17.release_rev%2520%2522CR%2520Fix%2520In%2520Ver%2522%252C%2520t18.title%2520%2522CR%2520Status%2522%252C%2520t0.x_Project_SL%2520%2522S%2526L%2520Number%2522%252C%2520CONVERT%2528CHAR%252C%2520t0.objid%2529%2520%2522objid%2522%2520FROM%2520table_case%2520t0%2520LEFT%2520OUTER%2520JOIN%2520table_mod_level%2520t2%2520ON%2520t0.case_prt2part_info%2520%253D%2520t2.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_part_num%2520t3%2520ON%2520t2.part_info2part_num%2520%253D%2520t3.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_close_case%2520t14%2520ON%2520t0.objid%2520%253D%2520t14.last_close2case%2520%2520LEFT%2520OUTER%2520JOIN%2520table_workaround%2520t15%2520ON%2520t0.case_soln2workaround%2520%253D%2520t15.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_probdesc%2520t16%2520ON%2520t15.workaround2probdesc%2520%253D%2520t16.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520mtm_probdesc12_bug28%2520m0%2520ON%2520m0.probdesc2bug%2520%253D%2520t16.objid%2520LEFT%2520OUTER%2520JOIN%2520table_bug%2520t17%2520ON%2520t17.objid%2520%253D%2520m0.bug2probdesc%2520LEFT%2520OUTER%2520JOIN%2520table_gbst_elm%2520t18%2520ON%2520t17.bug_sts2gbst_elm%2520%253D%2520t18.objid%2520%252C%2520table_act_entry%2520t1%252C%2520table_user%2520t4%252C%2520table_condition%2520t5%252C%2520table_gbst_elm%2520t6%252C%2520table_gbst_elm%2520t7%252C%2520table_site%2520t8%252C%2520table_gbst_elm%2520t9%252C%2520table_gbst_elm%2520t10%252C%2520table_contact%2520t11%252C%2520table_user%2520t12%252C%2520table_bus_org%2520t13%2520WHERE%2520%2528t1.act_entry2case%2520%253D%2520t0.objid%2529%2520AND%2520%2528t0.case_owner2user%2520%253D%2520t4.objid%2529%2520AND%2520%2528t0.case_state2condition%2520%253D%2520t5.objid%2529%2520AND%2520%2528t0.casests2gbst_elm%2520%253D%2520t6.objid%2529%2520AND%2520%2528t0.calltype2gbst_elm%2520%253D%2520t7.objid%2529%2520AND%2520%2528t0.case_reporter2site%2520%253D%2520t8.objid%2529%2520AND%2520%2528t8.primary2bus_org%2520%253D%2520t13.objid%2529%2520AND%2520%2528t0.respprty2gbst_elm%2520%253D%2520t9.objid%2529%2520AND%2520%2528t0.respsvrty2gbst_elm%2520%253D%2520t10.objid%2529%2520AND%2520%2528t0.case_reporter2contact%2520%253D%2520t11.objid%2529%2520AND%2520%2528t0.case_originator2user%2520%253D%2520t12.objid%2529%2520AND%2520%2528t1.entry_time%2520between%2520%2528GetDate%2528%2529%2520-%2520Convert%2528int%252C%2520%257B0%257D%2529%2529%2520AND%2520GetDate%2528%2529%2520AND%2520t3.part_number%2520like%2520%257B1%257D%2529&strClauses=%255B%2522update%257C268456593%257C268436064%257Cwithin%2520last%2520%2528days%2529%257C3%257C0%257C3%257Ccase_act2act_entry%253Aentry_time%257C1%2522%252C%2522update%257C268456594%257C0%257Cjoins%2520groups%257CAND%257C0%257C0%257C%257C2%2522%252C%2522update%257C268456595%257C268436071%257Cstarts%2520with%257CMC%257C0%257C0%257C%2528LEFT%2520OUTER%2529%2520case_prt2part_info%253Apart_info2part_num%253Apart_number%257C3%2522%255D";
+	case "Last2Days":
+		$fields_string = "query=268440603&obj_type=case&parameters=2%2CMC%25&sqlStmt=SELECT%2520t0.id_number%2520%2522Case%2520ID%2522%252C%2520t4.login_name%2520%2522Owner%2522%252C%2520t0.title%2520%2522Case%2520Title%2522%252C%2520t5.title%2520%2522Condition%2522%252C%2520t6.title%2520%2522Status%2522%252C%2520t7.title%2520%2522Case%2520Type*%2522%252C%2520t8.site_id%2520%2522Site%2520ID%2522%252C%2520t8.name%2520%2522Site%2520Name%2522%252C%2520t9.title%2520%2522Priority*%2522%252C%2520t10.title%2520%2522Severity*%2522%252C%2520t11.first_name%2520%2522Contact%2520First%2520Name%2522%252C%2520t11.last_name%2520%2522Contact%2520Last%2520Name%2522%252C%2520CONVERT%2528CHAR%252C%2520t0.creation_time%2529%2520%2522CONPREFIX_Create%2520Date%2522%252C%2520t12.login_name%2520%2522Originator%2522%252C%2520t13.org_id%2520%2522Org%2520ID%2522%252C%2520t8.region%2520%2522Site%2520Region%2522%252C%2520CONVERT%2528CHAR%252C%2520t14.close_date%2529%2520%2522CONPREFIX_Close%2520Date%2522%252C%2520t0.x_customer_issue_no%2520%2522Cust%2520Issue%2522%252C%2520t3.part_number%2520%2522Part%2520Number%2522%252C%2520t0.x_module%2520%2522Module%2522%252C%2520t17.id_number%2520%2522CR%2520ID%2522%252C%2520t17.release_rev%2520%2522CR%2520Fix%2520In%2520Ver%2522%252C%2520t18.title%2520%2522CR%2520Status%2522%252C%2520t0.x_Project_SL%2520%2522S%2526L%2520Number%2522%252C%2520CONVERT%2528CHAR%252C%2520t0.objid%2529%2520%2522objid%2522%2520FROM%2520table_case%2520t0%2520LEFT%2520OUTER%2520JOIN%2520table_mod_level%2520t2%2520ON%2520t0.case_prt2part_info%2520%253D%2520t2.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_part_num%2520t3%2520ON%2520t2.part_info2part_num%2520%253D%2520t3.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_close_case%2520t14%2520ON%2520t0.objid%2520%253D%2520t14.last_close2case%2520%2520LEFT%2520OUTER%2520JOIN%2520table_workaround%2520t15%2520ON%2520t0.case_soln2workaround%2520%253D%2520t15.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_probdesc%2520t16%2520ON%2520t15.workaround2probdesc%2520%253D%2520t16.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520mtm_probdesc12_bug28%2520m0%2520ON%2520m0.probdesc2bug%2520%253D%2520t16.objid%2520LEFT%2520OUTER%2520JOIN%2520table_bug%2520t17%2520ON%2520t17.objid%2520%253D%2520m0.bug2probdesc%2520LEFT%2520OUTER%2520JOIN%2520table_gbst_elm%2520t18%2520ON%2520t17.bug_sts2gbst_elm%2520%253D%2520t18.objid%2520%252C%2520table_act_entry%2520t1%252C%2520table_user%2520t4%252C%2520table_condition%2520t5%252C%2520table_gbst_elm%2520t6%252C%2520table_gbst_elm%2520t7%252C%2520table_site%2520t8%252C%2520table_gbst_elm%2520t9%252C%2520table_gbst_elm%2520t10%252C%2520table_contact%2520t11%252C%2520table_user%2520t12%252C%2520table_bus_org%2520t13%2520WHERE%2520%2528t1.act_entry2case%2520%253D%2520t0.objid%2529%2520AND%2520%2528t0.case_owner2user%2520%253D%2520t4.objid%2529%2520AND%2520%2528t0.case_state2condition%2520%253D%2520t5.objid%2529%2520AND%2520%2528t0.casests2gbst_elm%2520%253D%2520t6.objid%2529%2520AND%2520%2528t0.calltype2gbst_elm%2520%253D%2520t7.objid%2529%2520AND%2520%2528t0.case_reporter2site%2520%253D%2520t8.objid%2529%2520AND%2520%2528t8.primary2bus_org%2520%253D%2520t13.objid%2529%2520AND%2520%2528t0.respprty2gbst_elm%2520%253D%2520t9.objid%2529%2520AND%2520%2528t0.respsvrty2gbst_elm%2520%253D%2520t10.objid%2529%2520AND%2520%2528t0.case_reporter2contact%2520%253D%2520t11.objid%2529%2520AND%2520%2528t0.case_originator2user%2520%253D%2520t12.objid%2529%2520AND%2520%2528t1.entry_time%2520between%2520%2528GetDate%2528%2529%2520-%2520Convert%2528int%252C%2520%257B0%257D%2529%2529%2520AND%2520GetDate%2528%2529%2520AND%2520t3.part_number%2520like%2520%257B1%257D%2529&strClauses=%255B%2522update%257C268456593%257C268436064%257Cwithin%2520last%2520%2528days%2529%257C3%257C0%257C3%257Ccase_act2act_entry%253Aentry_time%257C1%2522%252C%2522update%257C268456594%257C0%257Cjoins%2520groups%257CAND%257C0%257C0%257C%257C2%2522%252C%2522update%257C268456595%257C268436071%257Cstarts%2520with%257CMC%257C0%257C0%257C%2528LEFT%2520OUTER%2529%2520case_prt2part_info%253Apart_info2part_num%253Apart_number%257C3%2522%255D";
+		break;
+	case "Last35Days":
+		$fields_string = "query=268440603&obj_type=case&parameters=35%2CMC%25&sqlStmt=SELECT%2520t0.id_number%2520%2522Case%2520ID%2522%252C%2520t4.login_name%2520%2522Owner%2522%252C%2520t0.title%2520%2522Case%2520Title%2522%252C%2520t5.title%2520%2522Condition%2522%252C%2520t6.title%2520%2522Status%2522%252C%2520t7.title%2520%2522Case%2520Type*%2522%252C%2520t8.site_id%2520%2522Site%2520ID%2522%252C%2520t8.name%2520%2522Site%2520Name%2522%252C%2520t9.title%2520%2522Priority*%2522%252C%2520t10.title%2520%2522Severity*%2522%252C%2520t11.first_name%2520%2522Contact%2520First%2520Name%2522%252C%2520t11.last_name%2520%2522Contact%2520Last%2520Name%2522%252C%2520CONVERT%2528CHAR%252C%2520t0.creation_time%2529%2520%2522CONPREFIX_Create%2520Date%2522%252C%2520t12.login_name%2520%2522Originator%2522%252C%2520t13.org_id%2520%2522Org%2520ID%2522%252C%2520t8.region%2520%2522Site%2520Region%2522%252C%2520CONVERT%2528CHAR%252C%2520t14.close_date%2529%2520%2522CONPREFIX_Close%2520Date%2522%252C%2520t0.x_customer_issue_no%2520%2522Cust%2520Issue%2522%252C%2520t3.part_number%2520%2522Part%2520Number%2522%252C%2520t0.x_module%2520%2522Module%2522%252C%2520t17.id_number%2520%2522CR%2520ID%2522%252C%2520t17.release_rev%2520%2522CR%2520Fix%2520In%2520Ver%2522%252C%2520t18.title%2520%2522CR%2520Status%2522%252C%2520t0.x_Project_SL%2520%2522S%2526L%2520Number%2522%252C%2520CONVERT%2528CHAR%252C%2520t0.objid%2529%2520%2522objid%2522%2520FROM%2520table_case%2520t0%2520LEFT%2520OUTER%2520JOIN%2520table_mod_level%2520t2%2520ON%2520t0.case_prt2part_info%2520%253D%2520t2.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_part_num%2520t3%2520ON%2520t2.part_info2part_num%2520%253D%2520t3.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_close_case%2520t14%2520ON%2520t0.objid%2520%253D%2520t14.last_close2case%2520%2520LEFT%2520OUTER%2520JOIN%2520table_workaround%2520t15%2520ON%2520t0.case_soln2workaround%2520%253D%2520t15.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520table_probdesc%2520t16%2520ON%2520t15.workaround2probdesc%2520%253D%2520t16.objid%2520%2520LEFT%2520OUTER%2520JOIN%2520mtm_probdesc12_bug28%2520m0%2520ON%2520m0.probdesc2bug%2520%253D%2520t16.objid%2520LEFT%2520OUTER%2520JOIN%2520table_bug%2520t17%2520ON%2520t17.objid%2520%253D%2520m0.bug2probdesc%2520LEFT%2520OUTER%2520JOIN%2520table_gbst_elm%2520t18%2520ON%2520t17.bug_sts2gbst_elm%2520%253D%2520t18.objid%2520%252C%2520table_act_entry%2520t1%252C%2520table_user%2520t4%252C%2520table_condition%2520t5%252C%2520table_gbst_elm%2520t6%252C%2520table_gbst_elm%2520t7%252C%2520table_site%2520t8%252C%2520table_gbst_elm%2520t9%252C%2520table_gbst_elm%2520t10%252C%2520table_contact%2520t11%252C%2520table_user%2520t12%252C%2520table_bus_org%2520t13%2520WHERE%2520%2528t1.act_entry2case%2520%253D%2520t0.objid%2529%2520AND%2520%2528t0.case_owner2user%2520%253D%2520t4.objid%2529%2520AND%2520%2528t0.case_state2condition%2520%253D%2520t5.objid%2529%2520AND%2520%2528t0.casests2gbst_elm%2520%253D%2520t6.objid%2529%2520AND%2520%2528t0.calltype2gbst_elm%2520%253D%2520t7.objid%2529%2520AND%2520%2528t0.case_reporter2site%2520%253D%2520t8.objid%2529%2520AND%2520%2528t8.primary2bus_org%2520%253D%2520t13.objid%2529%2520AND%2520%2528t0.respprty2gbst_elm%2520%253D%2520t9.objid%2529%2520AND%2520%2528t0.respsvrty2gbst_elm%2520%253D%2520t10.objid%2529%2520AND%2520%2528t0.case_reporter2contact%2520%253D%2520t11.objid%2529%2520AND%2520%2528t0.case_originator2user%2520%253D%2520t12.objid%2529%2520AND%2520%2528t1.entry_time%2520between%2520%2528GetDate%2528%2529%2520-%2520Convert%2528int%252C%2520%257B0%257D%2529%2529%2520AND%2520GetDate%2528%2529%2520AND%2520t3.part_number%2520like%2520%257B1%257D%2529&strClauses=%255B%2522update%257C268456593%257C268436064%257Cwithin%2520last%2520%2528days%2529%257C3%257C0%257C3%257Ccase_act2act_entry%253Aentry_time%257C1%2522%252C%2522update%257C268456594%257C0%257Cjoins%2520groups%257CAND%257C0%257C0%257C%257C2%2522%252C%2522update%257C268456595%257C268436071%257Cstarts%2520with%257CMC%257C0%257C0%257C%2528LEFT%2520OUTER%2529%2520case_prt2part_info%253Apart_info2part_num%253Apart_number%257C3%2522%255D";
 		break;
 	}
 	
