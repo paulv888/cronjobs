959fd338a2a8a6a63719a259059da404f8479874
paulv888 <paulv888@inbox.com>
2015-11-15 02:51:32 -0600

    Voice Commands

---
+++ includes1.php
@@ -0,0 +1,37 @@
+﻿<?php 
+include_once 'logins.php' ;
+include_once 'connect-db.php';
+include_once 'defines.php';
+
+
+include_once 'process1.php';
+
+include_once 'includes/remote.php';
+include_once 'shared/shared_db.php';
+include_once 'shared/shared_file.php';
+include_once 'shared/shared_ha.php';
+include_once 'shared/shared_gen.php';
+include_once 'includes/properties_factory.php';
+include_once 'includes/commands_factory.php';
+include_once 'includes/sendmail.php';
+include_once 'includes/timers.php';
+include_once 'includes/commands_weather.php';
+include_once 'includes/RT-N65U.php';
+include_once 'shared/simple_html_dom.php';
+include_once 'includes/monitor_devices.php';
+include_once 'includes/commands_thermo.php';
+include_once 'includes/thermo_common.php';
+include_once 'includes/alerts_common.php';
+
+include_once 'myclasses/thermo_lib.php';
+include_once 'myclasses/RestClient.class.php';
+include_once 'myclasses/TCPClient.php';
+include_once 'myclasses/insteon_hub.class.php';
+include_once 'myclasses/insteon_decoder.class.php';
+include_once 'myclasses/sockettransport.class.php';
+include_once 'myclasses/class.phpmailer.php' ;
+
+include_once 'myclasses/Orders.class.php';
+include_once 'myclasses/Position.class.php';
+include_once 'includes/yahoostreamer.php';
+?>
