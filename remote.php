895b19c78a3d2eb6ed4198c035f989c3eaacc4e3
PaulV888 <paul.vloon@outlook.com>
2026-01-08 20:26:17 -0600

    undo serviceworker stuff

--- remote.php
+++ remote.php
@@ -8,7 +8,14 @@ if (isset($_GET['key'])) {
     }
 	$apikey = $_GET['key'];
 } else {
-	die('Not Authorized');
+    $apikey='6785157a-27e6-483f-a52c-d73b7529fa21';
+	$found  = FetchRow('SELECT username FROM ha_mi_remote_users WHERE inuse = 1 and `apikey`="'.$apikey.'"');
+	if ($found) {
+		$username = $found['username'];
+	} else {
+	    die('Not Authorized'); 
+    }
+//	die('Not Authorized');
 }
 if (isset($_GET['name'])) {
 	if (!$remoteID = FetchRow('SELECT id FROM ha_remote_remotes WHERE UCASE(`description`)="'.strtoupper($_GET['name']).'"')['id']) die('Invalid Remote');
@@ -29,14 +36,24 @@ if (isset($_GET['name'])) {
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=3">
 <meta name="mobile-web-app-capable" content="yes" />
 <meta name="theme-color" content="#E9ECE5"/>
+<link rel="manifest" href="/manifest.json">
 <link rel="stylesheet" href="/media/templates/site/mod-cassiopeia/css/template.css" type="text/css" media="screen" /> 
 <link rel="stylesheet" href="/media/templates/site/mod-cassiopeia/css/user.css" type="text/css" media="screen" /> 
 <link rel="stylesheet" href="/media/templates/site/mod-cassiopeia/css/global/colors_alternative.css" type="text/css" media="screen" /> 
 <link rel="stylesheet" href="/media/system/css/joomla-fontawesome.css" type="text/css" media="screen" /> 
 <script src="/media/vendor/jquery/js/jquery.min.js"></script>
 <script async src="/templates/mod-cassiopeia/js/bstrap3.min.js"></script>
-<script async src="<?php echo getPath(true);?>/js/remote.js"></script>
+<script src="<?php echo getPath(true);?>/js/remote.js"></script>
+<script>
+// if ('serviceWorker' in navigator) {
+  // navigator.serviceWorker.register('<?php echo getPath(true);?>/js/service-worker.js')
+    // .then(reg => console.log('Service worker registered:',reg))
+    // .catch(err => console.log('SW registration failed:',err));
+// }
+</script>
 </head>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js"></script>
 <body style="padding:0px;">
 <noscript>
     <div style="position: fixed; top: 0px; left: 0px; z-index: 3000; height: 100%; width: 100%; background-color: #E9ECE5">
@@ -44,6 +61,7 @@ if (isset($_GET['name'])) {
     </div>
 </noscript>
    <div id='system-message-container'></div>
+   <script src="js/app.js"></script>
 <?php
    require_once 'includes.php'; 
    echo '<div class="keyscell" style="background-color: #222; color:#fff"> '.greeting().' '.$username.'</div>';
